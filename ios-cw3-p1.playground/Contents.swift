d8a944699dbf1e10ba4f33cf20df1852fd502d64
kymalj <70738114+kymalj@users.noreply.github.com>
2020-09-16 20:44:14 -0700

    done

---
+++ ios-cw3-p1.playground/Contents.swift
@@ -0,0 +1,7 @@
+func printme()
+{
+    print("my name: khalifa")
+    print("my age: 17")
+
+}
+printme()
---
+++ ios-cw3-p2.playground/Contents.swift
@@ -0,0 +1,9 @@
+var number1 = 1
+var number2 = 2
+func add(number1: Int , number2: Int) ->Int
+{
+    number1 + number2
+}
+var functionCall = add(number1: number1, number2: number2)
+
+print(functionCall)
